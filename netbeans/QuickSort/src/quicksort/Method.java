49c948a6f28fb149252033d1ecb4a909bbb7f8a2
sdariza <sdariza@uninorte.edu.co>
2023-11-07 00:48:10 -0500

    feat: ✨add quickSort into netbeans folder

---
+++ netbeans/QuickSort/src/quicksort/Method.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quicksort;
+
+/**
+ *
+ * @author USER
+ */
+public class Method {
+
+    public void printVector(int v[], int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(v[i] + "|");
+        }
+        System.out.println("");
+    }
+
+    public int rightMove(int v[], int i, int p) {
+        if (v[i] < v[p]) {
+            return rightMove(v, i + 1, p);
+        }
+        return i;
+    }
+
+    public int leftMove(int v[], int j, int p) {
+        if (v[j] > v[p]) {
+            return leftMove(v, j - 1, p);
+        }
+        return j;
+    }
+
+    public void swapping(int v[], int i, int j) {
+        int t = v[i];
+        v[i] = v[j];
+        v[j] = t;
+    }
+
+    public void quickSort(int v[], int first, int last) {
+        if (first < last) {
+            int i = first, j = last, p = (first + last) / 2;
+            i = rightMove(v, i, p);
+            j = leftMove(v, j, p);
+            if (i < j) {
+                swapping(v, i, j);
+                i++;
+                j--;
+            } else if (i == j) {
+                i++;
+                j--;
+            }
+            if (first < j) {
+                quickSort(v, first, j);
+            }
+            if (i < last) {
+                quickSort(v, i, last);
+            }
+
+        }
+    }
+
+}
---
+++ netbeans/QuickSort/src/quicksort/QuickSort.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quicksort;
+
+/**
+ *
+ * @author USER
+ */
+public class QuickSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Method myMethods = new Method();
+        int v[] = {-9999, 9, 8, 7, 61, 5, 4, 3, 21, 10, 11};
+        int n = v.length - 1;
+        System.out.println("Vector a ordenar");
+        myMethods.printVector(v, n);
+        myMethods.quickSort(v, 1, n);
+        System.out.println("Vector ordenado");
+        myMethods.printVector(v, n);
+    }
+
+}
